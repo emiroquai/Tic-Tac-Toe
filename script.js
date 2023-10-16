22d72acd6e2ae36f5f35637fabcaf891c8ed0543
emiroquai <22440776+emiroquai@users.noreply.github.com.>
2023-10-16 20:19:43 +0200

    Added makemove and gameflow controller functions

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
+//Gameboard module
 const Gameboard = (() => {
     const board = ['X', 'O', 'X', '', '', '', '', '', '']
+    const cells = document.querySelectorAll('div.cell')
 
     const getBoard = () => board;
 
-    //Make move function
-    const makeMove = (index, playerSymbol) => {
-    //logic to make move
-        board[index] = playerSymbol;
+    //Update display function
+    const displayBoard = () => {
+        cells.forEach((cell, index) => {
+            cell.dataset.index = index;
+            cell.textContent = board[index];
+        })
     }
-    //Update gameboard function
+    
+    //Game flow controller
+    const activePlayer = () => {
+        const x = Player("Player1", "X")
+        const o = Player("Player2", "O")
+        let isPlaying = x
+
+        cells.forEach((cell) => {
+            cell.addEventListener('click', () => {
+                if (isPlaying === x) {
+                    isPlaying = o;
+                } else {
+                    isPlaying = x;
+                }
+            })
+        })
+        return isPlaying
+    }
+
+    //Make move function
+    const makeMove = () => {
+        cells.forEach((cell, index) => {
+            cell.dataset.index = index;
+            cell.addEventListener('click', () => {
+                board[index] = activePlayer().playerSymbol;
+                update();
+            })
+        })
+    }
 
     //Check for a winner function
 
+    //Update Gameboard
+    const update = () => {
+        Gameboard.displayBoard();
+        Gameboard.activePlayer();
+        Gameboard.makeMove();
+    }
+
     return {
         getBoard,
-        makeMove
+        activePlayer,
+        makeMove,
+        displayBoard,
+        update
     };
 })();
+
+Gameboard.update();
 
 //Player factory
 function Player (name, playerSymbol) {
